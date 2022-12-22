7adf30a81cc4c012dc525e4861c99ea8f0c9269b
Syed Atyab Hussain <syed.atyab@codeline.rihal.om>
2022-12-22 09:39:36 +0400

    Created a new class to find pairs

--- src/MatchMaking.java
+++ src/MatchMaking.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -12,8 +14,31 @@ public class MatchMaking {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
+        int[] arrayOfNumbers = new int[] {6, 7, 4, 3, 4, 5, 6};
+        int target = 10;
 
+    }
+    
+    /**
+     * This method will process an array of integers and will generate
+     * pairs that will add up to a specific sum.
+     * 
+     * For Example:
+     * If the input is: {6, 7, 4, 3, 4, 5, 6}
+     * Then the output would be: [[6,4], [7,3], [4,6]]
+     * 
+     * Note: The order of numbers inside each pair doesn't matter.
+     * For Example:
+     * This [[6,4], [7,3], [4,6]] can be represented as [[4,6], [3,7], [6,4]]
+     * 
+     * @param inputArray: Collection of numbers.
+     * @param targetSum: Number that would e acquired by adding up a pair.
+     * @return
+     */
+    public ArrayList<Integer[]> getMatchingPairs(int[] inputArray, int targetSum) {
+        
+        
+        return new ArrayList<Integer[]>();
     }
 
 }
